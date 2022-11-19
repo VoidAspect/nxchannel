f6f8678a37119bb906b14e23d95e028ecce87318
Mykhailo Horbunov <mykhailo.horbunov@auto1.com>
2022-11-19 12:49:58 +0200

    nxchannel-web module initial commit

---
+++ nxchannel-web/src/main/java/com/alevel/java/nxchannel/nxchannelweb/NxchannelWebApplication.java
@@ -0,0 +1,13 @@
+package com.alevel.java.nxchannel.nxchannelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxchannelWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxchannelWebApplication.class, args);
+	}
+
+}
